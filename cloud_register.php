f3762211ae2ace10d4407da408f1fe9bb2a5fae7
Edward Andrés Ramirez Mesa <tantaroth@gmail.com>
2012-10-13 23:42:12 -0500

    nuevas funciones

---
+++ cloud_register.php
@@ -0,0 +1,14 @@
+<fieldset>
+	<legend align="right">Registrese</legend>
+	<article>
+		<form align="center">
+			<div> <label>Nombre</label> <input type="text"> </div>
+			<div><label>Apellido</label><input type="text"></div>
+			<div><label>Correo</label><input type="text"></div>
+			<div><label>Usuario</label><input type="text"></div>
+			<div><label>Clave</label><input type="password"></div>
+			<div><label>Confirma Clave</label><input type="password"></div>
+			<div align="right"><input type="button" value="Registrar!" onClick="$('#messageModal').slideDown();"></div>
+		</form>
+	</article>
+</fieldset>
---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>::: FINANCIAS :::</title>
+		<link rel=StyleSheet href="lib/css/clasica.css" type="text/css">
+		<script type="text/javascript" src="lib/js/jquery.min.js"></script>
+		<script type="text/javascript" src="lib/js/general.js"></script>
+	</head>
+	<body>
+		<div id="messageModal" align="center">Loading... <br>asdasdas<br>asdasdas<br>asdasdas<span class="close"><a href="javascript: $('#messageModal').slideUp();">X</a></span></div>
+		<section>
+			<nav>
+				<ul>
+					<li><a href="#main">inicio</a></li>
+					<li><a href="#register">Registrate!</a></li>
+					<li><a href="#access">Accede</a></li>
+				</ul>
+			</nav>
+			<header id="head">Financias</header>
+			<article id="content">
+				&nbsp;
+			</article>
+			<footer>&copy; Krainic 2012. Todos los derechos reservados. </footer>
+		</section>
+	</body>
+</html>
