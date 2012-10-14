4abbfc875e2b51d315c21dcc84921e2895759e16
Edward Andrés Ramirez Mesa <tantaroth@gmail.com>
2012-10-14 00:21:33 -0500

    actualizaciones

--- cloud_access.php
+++ cloud_access.php
@@ -1,1 +1,15 @@
-ACCESS!
+<html>
+	<head></head>
+	<body>
+		<fieldset>
+			<legend align="right">Accede</legend>
+			<article>
+				<form align="center">
+					<div><label>Usuario / Correo</label><input type="text"></div>
+					<div><label>Clave</label><input type="password"></div>
+					<div align="right"><input type="button" value="Acceder" onClick="$('#messageModal').slideDown();"></div>
+				</form>
+			</article>
+		</fieldset>
+	</body>
+</html>
--- cloud_register.php
+++ cloud_register.php
@@ -1,14 +1,19 @@
-<fieldset>
-	<legend align="right">Registrese</legend>
-	<article>
-		<form align="center">
-			<div> <label>Nombre</label> <input type="text"> </div>
-			<div><label>Apellido</label><input type="text"></div>
-			<div><label>Correo</label><input type="text"></div>
-			<div><label>Usuario</label><input type="text"></div>
-			<div><label>Clave</label><input type="password"></div>
-			<div><label>Confirma Clave</label><input type="password"></div>
-			<div align="right"><input type="button" value="Registrar!" onClick="$('#messageModal').slideDown();"></div>
-		</form>
-	</article>
-</fieldset>
+<html>
+	<head></head>
+	<body>
+		<fieldset>
+			<legend align="right">Registrate!</legend>
+			<article>
+				<form align="center">
+					<div> <label>Nombre</label> <input type="text"> </div>
+					<div><label>Apellido</label><input type="text"></div>
+					<div><label>Correo</label><input type="text"></div>
+					<div><label>Usuario</label><input type="text"></div>
+					<div><label>Clave</label><input type="password"></div>
+					<div><label>Confirma Clave</label><input type="password"></div>
+					<div align="right"><input type="button" value="Registrar!" onClick="$('#messageModal').slideDown();"></div>
+				</form>
+			</article>
+		</fieldset>
+	</body>
+</html>
